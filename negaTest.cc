db994995d462342aa45a94f1deddb3e63983a27c
Luis Miranda <lcmm1993@gmail.com>
2014-05-27 15:30:33 -0430

    Implementado minimax y minimax con alpha/ß

--- negaTest.cc
+++ negaTest.cc
@@ -5,11 +5,11 @@ using namespace std;
 int main(){
   state_t init;
   bool curPlay = false;
-  for(int i=0;i<28;++i){
+  for(int i=0;i<31;++i){
     init = init.move(!curPlay,PV[i]);
     curPlay = !curPlay;
   }
   Node<state_t> i(init,curPlay);
-  int n = negaMax(i,5);
+  int n = negaMax(i,2);
   cout << n << endl;
 }
